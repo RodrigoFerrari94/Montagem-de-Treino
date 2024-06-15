eb394d67690ac645ada7196b9990cbee654644b9
Rodrigo Ferrari <rodrigoferrari94rf@gmail.com>
2024-06-15 13:42:40 -0300

    Update paginas.js

--- paginas.js
+++ paginas.js
@@ -73,12 +73,11 @@ function gerarPDF() {
   var inputs = document.querySelectorAll("input");
   var timers = document.querySelectorAll("div.timer-tempo");
   var videos = document.querySelectorAll("div.video-container");
-  var legend = document.querySelectorAll("legend");
-  var field = document.querySelectorAll("fieldset");
 
-  var ocultarHistorico = document.getElementById("historico");
+  var field = document.querySelector("fieldset");
+
   var select = document.querySelector("select");
-  var a = document.querySelector("a");
+  var a = document.querySelectorAll("a");
 
   buttons.forEach(function (button) {
     button.style.display = "none";
@@ -95,8 +94,7 @@ function gerarPDF() {
   });
   select.style.display = "none";
   a.style.display = "none";
-  legend.style.display = "none";
-  ocultarHistorico.style.display = "none";
+
   field.style.display = "none";
 
   var element = document.querySelector("body");
@@ -117,8 +115,7 @@ function gerarPDF() {
     });
     select.style.display = "";
     a.style.display = "";
-    legend.style.display = "";
-    ocultarHistorico.style.display = "";
+
     field.style.display = "";
   });
   location.reload();
