26e2c51409923b3d09895a9b5c1a3d426f2c4500
Rodrigo Ferrari <rodrigoferrari94rf@gmail.com>
2024-06-15 16:11:30 -0300

    .

--- paginas.js
+++ paginas.js
@@ -71,7 +71,7 @@ function salvarEdicao() {
 function gerarPDF() {
   var element = document.querySelector("body");
   var ocultarElementos = document.querySelectorAll(
-    "button, input, div.timer-tempo, div.video-container, div#contatos, select#visualizarsecao, fieldset, a"
+    "button, input, div.timer-tempo, div.video-container, div#contatos, select, fieldset, a"
   );
 
   ocultarElementos.forEach(function (elem) {
@@ -83,6 +83,7 @@ function gerarPDF() {
       elem.classList.remove("oculto");
     });
   });
+  listar();
 }
 function iniciarContador(index) {
   var buttonTimer = document.getElementById(`buttonTimer-${index}`);
